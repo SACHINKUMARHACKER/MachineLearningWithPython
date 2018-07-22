bc4a044ed2c2fa7b9175c51651803abe485ed2e7
SACHINKUMARHACKER <sachinkum123567@gmail.com>
2018-07-21 23:05:16 -0700

    sin wave

--- demo/demo.py
+++ demo/demo.py
@@ -3,3 +3,13 @@ import pandas as pd
 
 df = pd.read_csv('E:\Git_Projects\machine learning\MachineLearningWithPython\iris.csv', header=None)
 df.head()
+
+#%%
+import matplotlib.pyplot as plt
+import matplotlib as mpl
+import numpy as np
+
+#%%
+x = np.linspace(0, 25, 100)
+plt.plot(x, np.cos(x))
+plt.show() 
